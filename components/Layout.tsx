4d852304c283e199bf2dc6b26c59a89d7da0cf08
Michael Bentley <michaelbentley95@gmail.com>
2022-09-29 16:05:07 -0400

    Set up a lot of styling stuff

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,41 +1,49 @@
-import React, { ReactNode } from 'react'
-import Link from 'next/link'
-import Head from 'next/head'
+import React, { ReactNode } from "react";
+import Link from "next/link";
+import Head from "next/head";
 
 type Props = {
-  children?: ReactNode
-  title?: string
-}
+    children?: ReactNode;
+    title?: string;
+};
 
-const Layout = ({ children, title = 'This is the default title' }: Props) => (
-  <div>
-    <Head>
-      <title>{title}</title>
-      <meta charSet="utf-8" />
-      <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-    </Head>
-    <header>
-      <nav>
-        <Link href="/">
-          <a>Home</a>
-        </Link>{' '}
-        |{' '}
-        <Link href="/about">
-          <a>About</a>
-        </Link>{' '}
-        |{' '}
-        <Link href="/users">
-          <a>Users List</a>
-        </Link>{' '}
-        | <a href="/api/users">Users API</a>
-      </nav>
-    </header>
-    {children}
-    <footer>
-      <hr />
-      <span>I'm here to stay (Footer)</span>
-    </footer>
-  </div>
-)
+const styles = {
+    wrapper: "flex min-h-screen flex-col justify-between",
+    navBar: "flex h-12 justify-between shadow-md items-center px-4",
+    navLink: "p-2",
+    footer: "flex justify-center items-center h-10 shadow-inner",
+};
 
-export default Layout
+const Layout = ({ children, title = "This is the default title" }: Props) => (
+    <div>
+        <Head>
+            <title>{title ? title + " - Amazona" : "Amazona"}</title>
+            <meta charSet="utf-8" />
+            <meta name="description" content="Ecommerce Website" />
+            <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        </Head>
+        <div className={styles.wrapper}>
+            <header>
+                <nav className={styles.navBar}>
+                    <Link href="/">
+                        <a className="text-lg font-bold">amazona</a>
+                    </Link>
+                    <div>
+                        <Link href="/cart">
+                            <a className={styles.navLink}>Cart</a>
+                        </Link>
+                        <Link href="/login">
+                            <a className={styles.navLink}>Login</a>
+                        </Link>
+                    </div>
+                </nav>
+            </header>
+            <div className="container m-auto mt-4 px-4">{children}</div>
+            <footer className={styles.footer}>
+                <span>This is a fake copyright footer</span>
+            </footer>
+        </div>
+    </div>
+);
+
+export default Layout;
